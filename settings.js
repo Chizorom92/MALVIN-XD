c3fd4d1adc358219901e7b84a921944099892a3c
Chizorom92 <bmaurice367@gmail.com>
2025-06-06 10:00:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ LINK_WHITELIST: "youtube.com,github.com",
         AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 
-        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY MALVIN XD 😆*",
+        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY BENJAMIN*",
 // set the auto reply massage on status reply  
 
 
